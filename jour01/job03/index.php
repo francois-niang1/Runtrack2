1122eebb615b177781047a3228185c50fa75ed03
francois-niang1 <francois.niang@laplateforme.io>
2021-11-02 10:57:57 +0100

    jour 1 terminé

--- jour01/job03/index.php
+++ jour01/job03/index.php
@@ -4,3 +4,50 @@ $float = 1.5;
 $str = "Salut !";
 $bool = true;
 
+echo '
+<!DOCTYPE >
+<html>
+    <head>
+    </head>
+    <body>
+        <table> <style>
+            table
+            {
+                border-collapse: collapse;
+            }
+            td
+            {
+                border: 1px solid black;
+            }</style>
+            <thead>
+                <tr>
+                    <td>Type</td>
+                    <td>Nom</td>
+                    <td>Valeur</td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Entier</td>
+                    <td>Int</td>
+                    <td>'.$int.'</td>
+                </tr>
+                <tr>
+                    <td>Chaine de caractère</td>
+                    <td>Str</td>
+                    <td>'.$str.'</td>
+                </tr>
+                <tr>
+                    <td>Flotant</td>
+                    <td>Float</td>
+                    <td>'.$float.'</td>
+                </tr>
+                <tr>
+                    <td>Booléen</td>
+                    <td>Bool</td>
+                    <td>'.$bool.'</td>
+                </tr>
+            </tbody>
+        </table>
+    </body>
+</html>';
