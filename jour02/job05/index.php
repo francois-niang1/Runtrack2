7a121fb45187b394daa90598a09e87d1709848fe
francois-niang1 <francois.niang@laplateforme.io>
2021-11-03 12:33:15 +0100

    Jour 2 termine

--- jour02/job05/index.php
+++ jour02/job05/index.php
@@ -1,3 +1,14 @@
 <?php
-$i = 0 ;
-
+$i = 1 ;
+$compteur = 0;
+$div = 1;
+for($i=0;$i<1000;$i++) {
+    $compteur = 0;
+    for($div=1;$div<=$i;$div++) {
+        if($i%$div==0){
+            $compteur++;
+        }
+    }
+    if ($compteur == 2)
+        echo $i.'<br/>';
+}
