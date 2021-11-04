5b7cc53b1faa1700c1a7fadfa57dcfe19b72d3a8
francois-niang1 <francois.niang@laplateforme.io>
2021-11-04 14:46:00 +0100

    Jour03 terminé

--- jour03/job07/index.php
+++ jour03/job07/index.php
@@ -1,1 +1,14 @@
 <?php
+$str ="Certaines choses changent, et d'autres ne changeront jamais.";
+$i=0;
+$j=1;
+
+while(isset($str[$j])==true){
+    $stock = $str[$i];
+    $str[$i] = $str[$j];
+    $str[$j] = $stock;
+    echo $str[$i];
+    $str[$i] = $str[$j];
+    $j++;
+}
+echo $str[$i];
