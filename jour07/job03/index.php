b0a74c12f0112b83d1fe3a46296e52c1ae5e4c42
francois-niang1 <francois.niang@laplateforme.io>
2021-11-10 09:54:33 +0100

    Job 04 fini

--- jour07/job03/index.php
+++ jour07/job03/index.php
@@ -1,3 +1,24 @@
 <?php
 
+session_start();
+
+$_SESSION['prenom'] .= ''.$_POST['prenom'];
+// .= permet de concatener DONC enchainer deux idées/strings entre elles (ne marche que sur les strings (?)) :: .= concatenating assignment operator ::: ex : 
+
+echo $_SESSION['prenom'];
+
+
+if (isset($_POST["reset"])){
+    session_destroy();
+}
+
 ?>
+
+<form action="index.php" method="post">
+    <div>
+        <label for="texte">prénom :</label>
+            <input type="text" id="prenom" name="prenom">
+    </div>
+    <input type="submit" value="submit" name="submit">
+    <input type="submit" value="reset" name="reset">
+</form>
